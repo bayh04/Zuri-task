5905411aab9161d62bbb6a7401c91460ee6609c0
bayh04 <ibrahimakintola20@gmail.com>
2022-06-26 10:05:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let firstName = "Ibrahim";
+let lastName = "Akintola";
+let myHeight = "1.65";
+let myCountry = "Nigeria";
+
+let fullName = firstName + " " + lastName;
+let myRealHeight = Number(myHeight);
+
+//using console.log function to output the code:
+
+console.log("Name:", fullName, "\nHeight:", myRealHeight, "meters", "\nCountry:", myCountry);
+
+//using document.write function to output the code:
+
+document.write("Name: ", fullName, "<br/><br/>");
+document.write("Height: ", myRealHeight, "<br/><br/>");
+document.write("Country: ", myCountry);
